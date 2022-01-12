db5914611e698090671d39752e8890d3ca0b65a7
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2022-01-11 23:45:53 -0500

    Consumo de API

--- src/api/prices.js
+++ src/api/prices.js
@@ -1,9 +1,34 @@
 import { API_HOST } from "../utils/contants";
+import { Dinero } from "dinero.js";
 
-export async function getPrices(currency) {
-  try {
-    const url = `${API_HOST}/latest?to=${currency}`;
-  } catch (error) {
-    throw error;
-  }
+export async function getCurrencies() {
+  return fetch(`https://${API_HOST}/currencies`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.error(error));
+}
+
+export async function getConvertion(amount, from, to) {
+  console.log(amount);
+  return fetch(
+    `https://${API_HOST}/latest?$amount=${amount}&from=${from}&to=${to}`
+  )
+    .then((resp) => resp.json())
+
+    .then((data) => {
+      PickerOptions();
+      return data;
+    })
+    .catch((err) => console.error(error));
+}
+
+function PickerOptions(results) {
+  const arrayKeys = Object.keys(results);
+
+  arrayKeys.forEach((item) => {
+    arrayPicker.push(<Picker.Item label={item} value={item} />);
+  });
+  return arrayPicker;
 }
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { getConvertion, getCurrencies } from "../api/prices";
 
 export default function Main() {
+  let arrayPicker = [];
+  useEffect(() => {
+    (async () => {
+      const results = await getCurrencies();
+      arrayPicker = PickerOptions(results);
+    })();
+  }, []);
+
+  const [selectedValue, setSelectedValue] = useState("");
+
+  function PickerOptions(results) {
+    const arrayKeys = Object.keys(results);
+
+    arrayKeys.forEach((item) => {
+      arrayPicker.push(<Picker.Item label={item} value={item} />);
+    });
+    return arrayPicker;
+  }
+  console.log(arrayPicker);
   return (
     <SafeAreaView>
       <Text>Main</Text>
+      <Picker
+        selectedvalue={selectedValue}
+        style={{ height: 50, width: 50 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        {arrayPicker}
+      </Picker>
+      {}
     </SafeAreaView>
   );
 }
